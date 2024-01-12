0d20a64aeb3b04ab2ef68cbe5ccb0054f66ad3e2
bodanesemateus <bodanesemateus@gmail.com>
2024-01-12 20:31:31 +0000

    criado interface para repository da origem

---
+++ src/domain/origem_carga_saida/repository/origem_carga_saida-repository.ts
@@ -0,0 +1,4 @@
+import RepositoryInterface from "../../.shared/repository/repository-interface";
+import OrigemCargaSaida from "../entity/origem_carga_saida";
+
+export default interface OrigemCargaSaidaRepositoryInterface extends RepositoryInterface<OrigemCargaSaida> {}
